542932283113b2ba90192ace2f1f09ef477f149c
M-Zain-924823 <mz924823@gmail.com>
2025-08-07 11:55:05 +0500

    I gave up on javascript

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-//Goal: 
-/*
-    - The div with class .display, will just be a display for the user
-    - For the backend logic, the actual variables will be used 
-    - DON'T TAKE VALUES OF VARIABLES FROM THE DISPLAY DIV
-    - Store each value and operator entered by user in a seperate backend logic inside vairables 
-       (forget about div display, it is just a display for the user, not for us programmers)
-*/
 let a=0, b, op, result;
 
 function add(a, b){
@@ -56,20 +48,6 @@ operators.forEach(operator => {
         display.textContent += op;
     });
 }); 
-/* Old Logic of the above function:
-        op = e.target.id;
-        display.textContent += op;
-        a = display.textContent;
-
-        a = a.slice(0, a.indexOf(op));
-        b = display.textContent;
-        b = b.slice(b.indexOf(op)+1);
-        if(b == "") {b=0;} 
-        else {b = b.slice(0, b.indexOf(op));}
-        a = Number(a);
-        b = Number(b);
-        display.textContent = operate(op, a, b) + op;
-*/
 
 equal.addEventListener("click", (e) => {
     b = display.textContent;
@@ -87,33 +65,3 @@ clear.addEventListener("click", (e) => {
     if(display.textContent == "") {
         display.textContent = 0; }
 });
-
-
-/*************************************************************************/
-// New Logic
-// numbers.forEach(number => {
-//     number.addEventListener("click", (e) => {
-//         a += e.target.id;
-//         printall(e.target.id);
-//     });
-// });
-
-// operators.forEach(operator => {
-//     operator.addEventListener("click", (e) => {
-//         op = e.target.id;
-//         temp = b;
-//         b = Number(a);
-//         a = 0;
-//         operate(op, temp, b);
-//         print(result + op);        
-//     });
-// });
-
-// function print(text){
-//     display.textContent = text;
-// }
-
-// function printall(text){
-//     if(display.textContent==="0") print(text);
-//     else display.textContent += text;
-// }
