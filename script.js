6ec5053dbe4a2f329820479b90c63d68b3527510
M-Zain-924823 <mz924823@gmail.com>
2026-01-31 16:35:04 +0500

    working of digits complete

--- script.js
+++ script.js
@@ -5,20 +5,22 @@ let multiply = (a, b) => a * b;
 let divide = (a, b) => a / b; // (b===0 ? 1 : b);
 let modulus = (a, b) => a % b;
 let result = 0;
-let a, b, operator;
+let a = 0, b = 0, operator;
 
 // getting dom elements
 let screen = document.querySelector(".screen");
 let digits = document.querySelectorAll(".digit");
 let operators = document.querySelectorAll(".operator");
-let equal = document.querySelector(".equal");
+// let equal = document.querySelector(".equal");
 let cross = document.querySelector("#cross");
 let clear = document.querySelector("#clear");
 let dot = document.querySelector(".dot");
 
 // calculate the result
 function operate(a, b, operator) {
-    switch(operator) {
+    let temp = operator;
+    operator = undefined;
+    switch(temp) {
         case '+': return add(a,b);
         case '-': return subtract(a,b);
         case '*': return multiply(a,b);
@@ -26,3 +28,18 @@ function operate(a, b, operator) {
         case '%': return modulus(a,b);
     }
 }
+
+// Working of each button
+digits.forEach(button => {
+    button.addEventListener("click", (e) => {
+        let digit = e.target.id;
+        if (operator === undefined) {
+            a += digit;
+            screen.textContent = +a;
+        } else {
+            b += digit;
+            screen.textContent = `${+a} ${operator} ${+b}`;
+        }
+    });
+});
+
